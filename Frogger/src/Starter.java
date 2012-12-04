c24178226b43bf74a2aab56f8a2b04285b3127ce
Robert Untalan <robertuntal@gmail.com>
2012-12-04 09:18:15 -0800

    For demonstration purpose added a hello file and changed the real starter

--- Frogger/src/Starter.java
+++ Frogger/src/Starter.java
@@ -4,9 +4,11 @@ public class Starter {
 	/**
 	 * @param args
 	 */
+	
+	/*
 	public static void main(String[] args) {
 		Game g = new Game(); 
-
+        System.out.println("hello world"); 
 	}
-
+*/
 }
