29d60442f8f301eb209652ece9c9191a03f8bc71
Robert Untalan <robertuntal@gmail.com>
2012-12-04 09:12:46 -0800

    New Project Frogger
    This will be baddass. 
    
    signed-off-by: robert Untalan <robertuntal@gmail.com>

---
+++ Frogger/src/Game.java
@@ -0,0 +1,19 @@
+
+public class Game {
+
+	/**
+	 * @param args
+	 */
+	public Game(){
+		
+		gw = new GameWorld(); 
+		play(); 
+	}
+	
+	private void play(){
+		
+		
+		
+	}
+
+}
---
+++ Frogger/src/GameWorld.java
@@ -0,0 +1,13 @@
+
+public class GameWorld {
+
+	/**
+	 * @param args
+	 */
+	
+	// code here to hold and 
+	 // manipulate world objects 
+	 // and related game state data
+	
+
+}
